7664e809d4e24700c095a8059afd3c241aa53dfd
Rudi MK <rudimk.cloud@gmail.com>
2021-06-05 18:17:11 +0530

    Added a script that gets all VMs on a running CloudStack cluster, checks if any of them have the snapshot=true KV tag, and snapshots them if the tag is found.

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+import schedule
+import time
+from cs import CloudStack
+
+
+def runSnapshotCycle():
+	cs = CloudStack(endpoint=os.getenv('ACS_API'), key=os.getenv('ACS_KEY'), secret=os.getenv('ACS_SECRET'))
+	# Grab a list of all VMs
+	allVMs = cs.listVirtualMachines(listall=True)
+	snapshotSources = []
+	# Find IDs of VMs that have the snapshot=true KV pair, and add them to the snapshot source list
+	for vm in allVMs['virtualmachine']:
+		for tags in vm['tags']:
+			if tags['key'] == 'snapshot' and tags['value'] == 'true':
+				snapshotSources.append(vm['id'])
+	# Now start making requests for creating VM snapshots for the VMs in the snapshot source list
+	for source in snapshotSources:
+		snapshot = cs.createVMSnapshot(virtualmachineid=source)
+		print(f"Creating snapshot: {snapshot}")
+
+
+if __name__ == '__main__':
+	runSnapshotCycle()
+
+
