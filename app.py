8aeaf60adfb37dbec7d119ba4518b8e1cc100d05
Rudi MK <rudimk.cloud@gmail.com>
2021-06-07 22:34:08 +0530

    Reversed the logic. We're now searching all tags and getting VM IDs that contain the 'snapshot' tag.

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ from cs import CloudStack
 
 def runSnapshotCycle():
 	cs = CloudStack(endpoint=os.getenv('ACS_API'), key=os.getenv('ACS_KEY'), secret=os.getenv('ACS_SECRET'))
-	# Grab a list of all VMs
-	allVMs = cs.listVirtualMachines(listall=True)
+	# Grab a list of all resources with the 'snapshot' tag in them.
+	allTags = cs.listTags(listall=True, key='snapshot')
 	snapshotSources = []
 	# Find IDs of VMs that have the snapshot=true KV pair, and add them to the snapshot source list
-	for vm in allVMs['virtualmachine']:
-		for tags in vm['tags']:
-			if tags['key'] == 'snapshot' and tags['value'] == 'true':
-				snapshotSources.append(vm['id'])
+	for resource in allTags['tag']:
+		snapshotSources.append(resource['resourceid'])
 	# Now start making requests for creating VM snapshots for the VMs in the snapshot source list
 	for source in snapshotSources:
 		snapshot = cs.createVMSnapshot(virtualmachineid=source)
